7c55f706e571702f77594c9d5867ae43759ba6cc
Luke Taylor <luke@deentaylor.com>
2023-04-05 11:37:45 -0400

    Display loading on front-end

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import type { Row } from './data/schema';
+import { dataPromise } from './data';
 
 import styles from './index.module.scss';
 import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
-import('./data').then(console.log);
 
 export default function App() {
+  const [rows, setRows] = useState<Row[] | null>(null);
+  dataPromise.then((data) => { setRows(data); });
+
   return (
     <div className={cx('base')}>
       <h1>CS178 Final Project</h1>
+      <p>{!rows ? 'Loading...' : `Got ${rows.length.toLocaleString()} rows`}</p>
     </div>
   );
 }
