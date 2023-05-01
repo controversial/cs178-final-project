670ce15c1707b05df75721cddd5ebb4a4347425c
Luke Taylor <luke@deentaylor.com>
2023-05-01 19:31:00 -0400

    heatmap draft 2

--- src/app/components/Heatmap/Heatmap.tsx
+++ src/app/components/Heatmap/Heatmap.tsx
@@ -1,6 +1,8 @@
 import React, { useRef, useEffect } from 'react';
 import * as d3 from 'd3';
 
+import type { Row } from '../../data/utils/schemas';
+import { gateNameSchema } from '../../data/utils/schemas';
 import rows from '../../data/sensor-readings';
 import adjacencyGraph from '../../data/sensor-adjacency-graph';
 
@@ -8,50 +10,29 @@ import classNames from 'classnames/bind';
 import styles from './Heatmap.module.scss';
 const cx = classNames.bind(styles);
 
+const trips = d3.group(rows, (r) => r.tripId);
+const freqs: Partial<Record<`${Row['gateName']}--${Row['gateName']}`, number>> = {};
+trips.forEach((tripRows) => {
+  tripRows.slice(0, -1).forEach((row, i) => {
+    const key = `${row.gateName}--${tripRows[i + 1]!.gateName}` as const;
+    freqs[key] = (freqs[key] ?? 0) + 1;
+  });
+});
+
 
 export default function Heatmap() {
-  const containerRef = useRef<SVGGElement | null>(null);
-
-  useEffect(() => {
-    if (!containerRef.current) return () => undefined;
-
-    const container = containerRef.current;
-    const tripsWithCoordinates = d3.map(rows, (r) => {
-      const { x, y } = adjacencyGraph[r.gateName];
-      return { ...r, x, y };
-    });
-    const trips = d3.group(tripsWithCoordinates, (d) => d.tripId);
-    // Create a path from each trip
-    const paths = Array.from(trips.values()).map((trip) => {
-      const path = d3.path();
-      if (!trip[0]) return null;
-      const jitter = () => Math.random() * 8 - 4;
-      path.moveTo(trip[0].x + jitter(), trip[0].y + jitter());
-      for (let i = 1; i < trip.length; i += 1) {
-        path.lineTo(trip[i]!.x + jitter(), trip[i]!.y + jitter());
-      }
-      return path;
-    });
-    // Add paths
-    d3.select(container)
-      .selectAll('path')
-      .data(paths)
-      .join('path')
-      .filter((d) => d != null)
-      .attr('d', (d) => d?.toString() ?? '')
-      .attr('stroke', 'rgb(255 0 0 / 0.05)')
-      .attr('stroke-width', 0.1)
-      .attr('fill', 'none');
-
-    return () => {
-      container.innerHTML = '';
-    };
-  }, []);
+  const widthScale = d3.scaleLinear().domain([0, d3.max(Object.values(freqs))!]).range([0, 20]);
 
   return (
     <div className={cx('base')}>
       <svg width="100%" height="100%" viewBox="0 0 200 200">
-        <g ref={containerRef} />
+        {Object.entries(freqs).map(([key, freq]) => {
+          const [from, to] = key.split('--').map((gn) => gateNameSchema.parse(gn));
+          const d = `M${adjacencyGraph[from!].x + 0.5},${adjacencyGraph[from!].y + 0.5} L${adjacencyGraph[to!].x + 0.5},${adjacencyGraph[to!].y + 0.5}`;
+          return (
+            <path key={key} d={d} fill="none" stroke="rgb(255 0 0 / 50%)" strokeWidth={widthScale(freq)} />
+          );
+        })}
       </svg>
 
       <img src="/basemap.bmp" alt="Base map of the Lekagul Preserve" />
