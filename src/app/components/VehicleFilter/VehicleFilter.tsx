59b1cfa0fa6b21c17be499025ebbbbfe7f2d958f
Luke Taylor <luke@deentaylor.com>
2023-05-06 19:32:03 -0400

    Clean up VehicleFilter markup

--- src/app/components/VehicleFilter/VehicleFilter.tsx
+++ src/app/components/VehicleFilter/VehicleFilter.tsx
@@ -2,24 +2,24 @@ import React from 'react';
 import { carTypes } from '../../data/utils/schemas';
 
 import styles from './VehicleFilter.module.scss';
-import classNames from 'classnames/bind';
+import classNames from 'classnames';
+import classNamesBinder from 'classnames/bind';
 import { useData } from '../DataProvider';
-const cx = classNames.bind(styles);
+const cx = classNamesBinder.bind(styles);
 
 
-function VehicleFilter({ ...props }: React.HTMLAttributes<HTMLElement>) {
+function VehicleFilter({ className, ...props }: React.HTMLAttributes<HTMLElement>) {
   const { toggleVehicleTypeFilter } = useData();
   return (
-    <div {...props}>
+    <div className={classNames(cx('base', className))} {...props}>
       <p>Filter by Vehicle Type</p>
-      <div className={cx('base')}>
-        {carTypes.map((carType) => (
-          <label className={cx('item')} key={carType}>
-            <input type="checkbox" onChange={(event) => toggleVehicleTypeFilter(carType, event.target.checked)} />
-            {carType}
-          </label>
-        ))}
-      </div>
+
+      {carTypes.map((carType) => (
+        <label className={cx('item')} key={carType}>
+          <input type="checkbox" onChange={(event) => toggleVehicleTypeFilter(carType, event.target.checked)} />
+          {carType}
+        </label>
+      ))}
     </div>
   );
 }
