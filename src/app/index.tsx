24b180254204271f21616a67a10e24870bbf7b9d
Luke Taylor <luke@deentaylor.com>
2023-05-06 18:01:45 -0400

    Start adding some charts to dashboard
    Co-authored-by: Sarah Svahn <sarahsvahn@users.noreply.github.com>
    Co-authored-by: Michael Simpson <kenziesimpson@users.noreply.github.com>

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,10 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useCallback } from 'react';
+import TripTime from './components/TripTime';
+import Histogram from './components/Histogram';
 
 import styles from './index.module.scss';
 import classNames from 'classnames/bind';
+import Heatmap from './components/Heatmap/Heatmap';
 const cx = classNames.bind(styles);
 
 const DataProvider = React.lazy(() => import('./components/DataProvider'));
@@ -12,7 +15,22 @@ export default function App() {
     <div className={cx('base')}>
       <Suspense fallback={<p>Loading...</p>}>
         <DataProvider>
-          ...
+          <Histogram
+            className={cx('month-histogram')}
+            accessor={useCallback((d) => d.timestamp.getUTCMonth(), [])}
+            bins={new Array(12).fill(0).map((_, i) => i)}
+          />
+          <Histogram
+            className={cx('time-histogram')}
+            accessor={useCallback((d) => d.timestamp.getUTCHours(), [])}
+            bins={new Array(24).fill(0).map((_, i) => i)}
+          />
+          <Heatmap
+            className={cx('heatmap')}
+          />
+          <TripTime
+            className={cx('selected-trips-time')}
+          />
         </DataProvider>
       </Suspense>
     </div>
