996d3e166644ea61721c4991e4898d9a282fc036
Luke Taylor <luke@deentaylor.com>
2023-05-06 17:58:48 -0400

    Allow className on wrapped charts

--- src/app/components/ChartWrapper/ChartWrapper.tsx
+++ src/app/components/ChartWrapper/ChartWrapper.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import useElementSize from '../../helpers/use-element-size';
 
-import classNames from 'classnames/bind';
+import classNames from 'classnames';
+import classNamesBinder from 'classnames/bind';
 import styles from './ChartWrapper.module.scss';
-const cx = classNames.bind(styles);
+const cx = classNamesBinder.bind(styles);
 
 
 export default function wrapChart<T>(
   Component: React.ComponentType<T & { width: number, height: number }>,
 ) {
-  return function WrappedChart(props: Omit<T, 'width' | 'height'>) {
+  return function WrappedChart({
+    className = undefined,
+    ...props
+  }: {
+    className?: string;
+  } & Omit<T, 'width' | 'height'>) {
     const [baseRef, [width, height]] = useElementSize();
     return (
-      <div className={cx('base')} ref={baseRef}>
+      <div className={classNames(cx('base'), className)} ref={baseRef}>
         {width != null && height != null && (
           <Component width={width} height={height} {...props as T} />
         )}
