904ca90e30898eb9c247f009156244e390e95472
Luke Taylor <luke@deentaylor.com>
2023-05-07 15:30:45 -0400

    Enable brush filtering for both histograms
    Co-authored-by: Michael Simpson <kenziesimpson@users.noreply.github.com>
    Co-authored-by: Sarah Svahn <sarahsvahn@users.noreply.github.com>

--- src/app/components/Dashboard/Dashboard.tsx
+++ src/app/components/Dashboard/Dashboard.tsx
@@ -14,7 +14,13 @@ const cx = classNames.bind(styles);
 
 export default function DashboardLayout() {
   const { filteredTrips } = useData();
+
   const selectedTrips = useGlobalStore((state) => state.selectedTrips);
+  const setDateFilter = useGlobalStore((state) => state.setDateFilter);
+  const clearDateFilter = useGlobalStore((state) => state.clearDateFilter);
+  const setTimeFilter = useGlobalStore((state) => state.setTimeFilter);
+  const clearTimeFilter = useGlobalStore((state) => state.clearTimeFilter);
+
   const visibleSelectedTrips = [...selectedTrips].filter((tripId) => filteredTrips.has(tripId));
 
   return (
@@ -25,12 +31,29 @@ export default function DashboardLayout() {
         accessor={useCallback((d) => d.timestamp.getUTCMonth(), [])}
         bins={new Array(12).fill(0).map((_, i) => i)}
         binLabels={new Array(12).fill(0).map((_, monthIdx) => new Date(2020, monthIdx).toLocaleString('default', { timeZone: 'UTC', month: 'short' }))}
+        onBrush={(startBinIndex, endBinIndex) => {
+          let endBinIndexCapped = endBinIndex; // checks for selecting out of range past Dec
+          if (endBinIndexCapped === 12) {
+            endBinIndexCapped = 11;
+          }
+          const startDate = new Date(Date.UTC(startBinIndex < 4 ? 2016 : 2015, startBinIndex, 1));
+          const endYear = endBinIndex < 4 ? 2016 : 2015;
+          const endDate = new Date(Date.UTC(
+            endBinIndexCapped === 11 ? endYear + 1 : endYear,
+            (endBinIndexCapped + 1) % 12,
+            0,
+          ));
+          setDateFilter(startDate, endDate);
+        }}
+        onClearBrush={clearDateFilter}
       />
       <Histogram
         label="Histogram: Hours"
         className={cx('time-histogram')}
         accessor={useCallback((d) => d.timestamp.getUTCHours(), [])}
         bins={new Array(24).fill(0).map((_, i) => i)}
+        onBrush={(startBinIndex, endBinIndex) => { setTimeFilter(startBinIndex, endBinIndex); }}
+        onClearBrush={clearTimeFilter}
       />
       <Heatmap
         className={cx('heatmap')}
