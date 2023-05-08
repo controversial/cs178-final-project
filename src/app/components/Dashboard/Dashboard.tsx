38ee4eb1a5b5362fa15982c3d8e816447a3e8f38
Luke Taylor <luke@deentaylor.com>
2023-05-08 03:19:06 -0400

    Better Dashboard histogram brush logic

--- src/app/components/Dashboard/Dashboard.tsx
+++ src/app/components/Dashboard/Dashboard.tsx
@@ -12,12 +12,59 @@ import styles from './Dashboard.module.scss';
 import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
+
+function MonthHistogram() {
+  const { allReadings } = useData();
+  // 100 to cut off stragglers
+  const firstDate = [
+    allReadings[100]!.timestamp.getUTCFullYear(),
+    allReadings[100]!.timestamp.getUTCMonth(),
+  ] as const;
+  const lastDate = [
+    allReadings.at(-100)!.timestamp.getUTCFullYear(),
+    allReadings.at(-100)!.timestamp.getUTCMonth(),
+  ] as const;
+
+  const months = new Array((lastDate[0] - firstDate[0]) * 12 + lastDate[1] - firstDate[1] + 1)
+    .fill(null)
+    .map((_, i) => new Date(Date.UTC(firstDate[0], firstDate[1] + i, 1)));
+  const monthBins = months.map((d) => `${d.getUTCFullYear()}-${d.getUTCMonth()}` as const);
+
+  const setDateFilter = useGlobalStore((state) => state.setDateFilter);
+  const clearDateFilter = useGlobalStore((state) => state.clearDateFilter);
+
+  return (
+    <Histogram
+      label="Histogram: Months"
+      className={cx('month-histogram')}
+      accessor={useCallback((d) => `${d.timestamp.getUTCFullYear()}-${d.timestamp.getUTCMonth()}`, [])}
+      bins={monthBins}
+      binLabels={months.map((d) => d.toLocaleString('default', { timeZone: 'UTC', month: 'short' }))}
+      onBrush={(start, end) => {
+        let startTimestamp: number = 0;
+        let endTimestamp: number = allReadings.at(-1)!.timestamp.getTime() + 1;
+        if (start) {
+          const d1 = months[start.binIdx]!;
+          const afterD1 = new Date(Date.UTC(d1.getUTCFullYear(), d1.getUTCMonth() + 1, 1));
+          startTimestamp = +d1 + (start.t * (+afterD1 - +d1));
+        }
+        if (end) {
+          const d2 = months[end.binIdx]!;
+          const afterD2 = new Date(Date.UTC(d2.getUTCFullYear(), d2.getUTCMonth() + 1, 1));
+          endTimestamp = +d2 + (end.t * (+afterD2 - +d2));
+        }
+        setDateFilter(new Date(startTimestamp), new Date(endTimestamp));
+      }}
+      onClearBrush={clearDateFilter}
+    />
+  );
+}
+
+
 export default function DashboardLayout() {
   const { filteredTrips } = useData();
 
   const selectedTrips = useGlobalStore((state) => state.selectedTrips);
-  const setDateFilter = useGlobalStore((state) => state.setDateFilter);
-  const clearDateFilter = useGlobalStore((state) => state.clearDateFilter);
   const setTimeFilter = useGlobalStore((state) => state.setTimeFilter);
   const clearTimeFilter = useGlobalStore((state) => state.clearTimeFilter);
 
@@ -25,34 +72,13 @@ export default function DashboardLayout() {
 
   return (
     <div className={cx('base', { 'selected-trips': visibleSelectedTrips.length > 0 })}>
-      <Histogram
-        label="Histogram: Months"
-        className={cx('month-histogram')}
-        accessor={useCallback((d) => d.timestamp.getUTCMonth(), [])}
-        bins={new Array(12).fill(0).map((_, i) => i)}
-        binLabels={new Array(12).fill(0).map((_, monthIdx) => new Date(2020, monthIdx).toLocaleString('default', { timeZone: 'UTC', month: 'short' }))}
-        onBrush={(startBinIndex, endBinIndex) => {
-          let endBinIndexCapped = endBinIndex; // checks for selecting out of range past Dec
-          if (endBinIndexCapped === 12) {
-            endBinIndexCapped = 11;
-          }
-          const startDate = new Date(Date.UTC(startBinIndex < 4 ? 2016 : 2015, startBinIndex, 1));
-          const endYear = endBinIndex < 4 ? 2016 : 2015;
-          const endDate = new Date(Date.UTC(
-            endBinIndexCapped === 11 ? endYear + 1 : endYear,
-            (endBinIndexCapped + 1) % 12,
-            0,
-          ));
-          setDateFilter(startDate, endDate);
-        }}
-        onClearBrush={clearDateFilter}
-      />
+      <MonthHistogram />
       <Histogram
         label="Histogram: Hours"
         className={cx('time-histogram')}
         accessor={useCallback((d) => d.timestamp.getUTCHours(), [])}
         bins={new Array(24).fill(0).map((_, i) => i)}
-        onBrush={(startBinIndex, endBinIndex) => { setTimeFilter(startBinIndex, endBinIndex); }}
+        onBrush={(startBin, endBin) => { setTimeFilter(startBin?.bin ?? -1, endBin?.bin ?? 25); }}
         onClearBrush={clearTimeFilter}
       />
       <Map
